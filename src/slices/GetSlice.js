c5d153df38e64f046501b1a8f9d602c8d834d839
Арнольд Закарян <sherlock97@Noutbuk-Arnold.local>
2025-04-08 14:30:34 -0700

    Сообщение о коммите

---
+++ src/slices/GetSlice.js
@@ -0,0 +1,28 @@
+import {  createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    return data; // returns the fetched data
+  });
+
+export const getSlice = createSlice({
+    name: 'GetSlice', 
+    initialState: [],
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+          .addCase(fetchUsers.pending, (state) => {
+            state.status = 'loading';
+          })
+          .addCase(fetchUsers.fulfilled, (state, action) => {
+            state.status = 'succeeded';
+            state.users = action.payload;
+          })
+          .addCase(fetchUsers.rejected, (state, action) => {
+            state.status = 'failed';
+            state.error = action.error.message;
+          });
+      },
+
+})
